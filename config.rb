d9937a9b9f71dafa8b8d2bf907696aa0effa21c5
Eric Meyer <eric@oddbird.net>
2012-11-18 16:34:53 -0700

    Basic setup

---
+++ config.rb
@@ -0,0 +1,18 @@
+# Compass CSS framework config file
+
+require 'susy'
+require 'modular-scale'
+require 'animate'
+
+project_type = :stand_alone
+# Set this to the root of your project when deployed:
+http_path = "/"
+sass_dir = "sass"
+css_dir = "content/media/css"
+images_dir = "content/media/images"
+fonts_dir = "content/media/fonts"
+javascripts_dir = "content/media/js"
+line_comments = false
+preferred_syntax = :scss
+output_style = :expanded
+relative_assets = true
